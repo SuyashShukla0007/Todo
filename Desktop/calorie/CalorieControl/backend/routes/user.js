3b036b548b4b05cebe6fd40fc6dca24a522407fd
suyash shukla <123103079@nitkkr.ac.in>
2024-10-06 01:24:52 +0530

    Delete Desktop directory

--- Desktop/calorie/CalorieControl/backend/routes/user.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { postUser , getUser} = require('../controller/user');
-router.post('/sign', postUser);
-router.get('/user',getUser)
-module.exports = router;
