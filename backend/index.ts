86d8f38c1816fbb236342ccf3217255eec325efe
suyash shukla <123103079@nitkkr.ac.in>
2024-10-09 23:31:23 +0530

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -6,8 +6,12 @@ import createUser from './controllers/user';
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.options('*', cors());  // Enable CORS preflight requests
-// allow all origins
+app.use(cors({
+  origin: 'https://todo-zs4f.vercel.app',  // Replace with the actual URL of your frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Specify allowed headers if necessary
+}));
+
 app.use(express.json());
 
 app.get('/todo/:UserId', getTodos);
